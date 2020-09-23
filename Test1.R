4f74d7d28f40c4a6d9b312d079dadcca96ed6ab7
unknown <chinku3571@gmail.com>
2020-09-23 12:46:37 +0530

    R studio , git hub path setup .:)

--- Test1.R
+++ Test1.R
@@ -1,1 +1,8 @@
-print("I am a Tester ")
+print("Tool->global option -> git/svn ->enable[ok]-> git exectable path ->apply ")
+print("create Repo in git hub copy path ")
+print("File-> new project -> version control -> path(github repo)-> ok ")
+print("File-> new File -> *.R ")
+print("Tool -> version control -> commit ->select -> commit message ->
+      commit [tab]-> push ")
+print("Success ")
+
